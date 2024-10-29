4f32be3f70d0b32994d9f4784cebe98cbcf22511
Sunanya-TechId456 <sunanyamondal456@gmail.com>
2024-10-29 11:11:39 +0530

    Add files via upload

---
+++ header/index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>header</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+    <div class="main-container">
+     <div class="container">
+        <div class="logo">Soner Hasel</div>
+        <a href="../slider/index.php"class="logout-button">Logout</a>
+
+     </div>
+    </div>
+</body>
+</html>
